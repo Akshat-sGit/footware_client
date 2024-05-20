40aae5146792303b5dd4578b51fc19e846064b8d
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-05-20 20:19:53 +0530

    login page created

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:footwear_client/pages/signin_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:footwear_client/utils/colors.dart';
+
+class MySplashScreen extends StatelessWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: size.height * 0.53,
+                width: size.width,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
+                  color: Colors.black,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "images/n4.png",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.6,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "Discover your \n Dream Footware",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          color: Colors.white,
+                          height: 1.2),
+                    ),
+                    const SizedBox(height: 25),
+                    Text(
+                      "Explore all the most exiting jobs roles\nbased on your interest And study major",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: textColor3.withOpacity(0.8),
+                      ),
+                    ),
+                    SizedBox(height: size.height * 0.07),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30,
+                      ),
+                      child: Container(
+                        height: size.height * 0.08,
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: backgroundColor3.withOpacity(0.9),
+                          border: Border.all(
+                            color: Colors.white,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12.withOpacity(0.05),
+                              spreadRadius: 1,
+                              blurRadius: 7,
+                              offset: const Offset(0, -1),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 5),
+                          child: Row(
+                            children: [
+                              Container(
+                                height: size.height * 0.08,
+                                width: size.width / 2.2,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Register",
+                                    style: GoogleFonts.poppins(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: textColor1,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const SignIn(),
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  "Sign In",
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: textColor1,
+                                  ),
+                                ),
+                              ),
+                              const Spacer(),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
