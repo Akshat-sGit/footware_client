6f46af0c509ff0300dacbbca75392e365384942a
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-05-28 18:13:28 +0530

    firebase options added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:footwear_client/firebase_options.dart';
 import 'package:footwear_client/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
