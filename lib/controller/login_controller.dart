daa200d7a20645d0418fdfc7b9eca34994e16f30
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-06-07 00:45:05 +0530

    created model folder where I made user

--- lib/controller/login_controller.dart
+++ lib/controller/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
@@ -11,5 +12,29 @@ class LoginController extends GetxController {
   void onInit() {
     userCollection = firestore.collection('users'); 
     super.onInit(); 
+  } 
+
+
+  addUser() {
+    try {
+      DocumentReference doc = userCollection.doc();
+      // Product product = Product(
+      //   id: doc.id,
+      //   name: nameController.text,
+      //   category: category,
+      //   description: descriptionController.text,
+      //   price: double.parse(priceController.text),
+      //   brand: brand,
+      //   image: imageController.text,
+      //   offer: offer,
+      // );
+      final produtJson = product.toJson();
+      doc.set(produtJson);
+      Get.snackbar('Success', 'Product added successfully',
+          colorText: Colors.green);
+    } catch (e) {
+      Get.snackbar('Error', e.toString(), colorText: Colors.red);
+    }
   }
+
 }
