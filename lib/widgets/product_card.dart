08edc5b13325399ed81c87f9295d546932dc19bd
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-06-30 23:50:16 +0530

    clean code

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:footwear_client/utils/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ProductCard extends StatelessWidget {
